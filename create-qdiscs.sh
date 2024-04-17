3e464d8ed90e83bd611d3c773b42c8ddbd9a04cd
Donald Hunter <donald.hunter@gmail.com>
2024-04-17 14:29:13 +0100

    Regenerate create-qdiscs.sh for latest tc spec

--- create-qdiscs.sh
+++ create-qdiscs.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash
-DP='mydp'
+#!/usr/bin/bash
 QDISCS=${QDISCS:-blackhole cake cbs choke codel drr etf ets pfifo bfifo pfifo_head_drop fq fq_codel \
        fq_pie noqueue pfifo_fast gred hfsc hhf htb ingress clsact mq mqprio multiq netem \
        pie plug prio qfq red sfb sfq taprio tbf}
@@ -29,52 +28,52 @@ parent() {
 options() {
     case $1 in
         cbs)
-            echo ", \"tca-options\": { \"parms\": \"00000000 00000000 00000000 00000000 00000000\" }"
+            echo ", \"options\": { \"parms\": \"00000000 00000000 00000000 00000000 00000000\" }"
             ;;
         choke)
-            echo ", \"tca-options\": { \"parms\": \"00000000 00000000 00000000 00000000\",
+            echo ", \"options\": { \"parms\": \"00000000 00000000 00000000 00000000\",
                     \"stab\": \"$(perl -e 'print "00" x 256')\" }"
             ;;
         etf)
-            echo ", \"tca-options\": { \"parms\": \"00000000 0b000000 00000000\" }"
+            echo ", \"options\": { \"parms\": \"00000000 0b000000 00000000\" }"
             ;;
         ets)
-            echo ", \"tca-options\": { \"nbands\": 1 }"
+            echo ", \"options\": { \"nbands\": 1 }"
             ;;
         gred)
-            echo ", \"tca-options\": { \"limit\": 1, \"dps\": \"01000000 00000000 00000000\" }"
+            echo ", \"options\": { \"limit\": 1, \"dps\": \"01000000 00000000 00000000\" }"
             ;;
         hfsc)
-            echo ", \"tca-options\": \"0000\""
+            echo ", \"options\": { }"
             ;;
         htb)
-            echo ", \"tca-options\": { \"init\": \"03000000 00000000 00000000 00000000 00000000\" }"
+            echo ", \"options\": { \"init\": { \"version\": 3 } }"
             ;;
         mqprio)
-            echo ", \"tca-options\": \"01$(perl -e 'print "00" x 16')0001$(perl -e 'print "00" x 63')\""
+            echo ", \"options\": { \"num-tc\": 1, \"count\": \"01$(perl -e 'print "00" x 31')\" }"
             ;;
         multiq)
-            echo ", \"tca-options\": \"01001000\""
+            echo ", \"options\": { \"bands\": 1, \"max-bands\": 16 }"
             ;;
         netem)
-            echo ", \"tca-options\": \"$(perl -e 'print "00" x 24')\""
+            echo ", \"options\": { } "
             ;;
         prio)
-            echo ", \"tca-options\": \"02000000$(perl -e 'print "00" x 16')\""
+            echo ", \"options\": { \"bands\": 2 }"
             ;;
         red)
-            echo ", \"tca-options\": { \"parms\": \"$(perl -e 'print "00" x 16')\", \
+            echo ", \"options\": { \"parms\": \"$(perl -e 'print "00" x 16')\", \
                     \"stab\": \"$(perl -e 'print "00" x 256')\" }"
             ;;
         taprio)
-            echo ", \"tca-options\": { \"sched-cycle-time\": 1000, \"sched-clockid\": 11,
+            echo ", \"options\": { \"sched-cycle-time\": 1000, \"sched-clockid\": 11,
                     \"sched-entry-list\": { \"entry\": { \"cmd\": 0, \"gate-mask\": 1, \"interval\": 500000 } },
                     \"priomap\": \"01$(perl -e 'print "00" x 16')0001$(perl -e 'print "00" x 63')\" },
-                    \"tca-stab\": {\"base\": \"00000000 10000000 00000000 00000000 00000000 00000000\" }"
-                    #\"tca-stab\": {\"base\": {\"overhead\": 24 } }"
+                    \"stab\": {\"base\": \"00000000 10000000 00000000 00000000 00000000 00000000\" }"
+                    #\"stab\": {\"base\": {\"overhead\": 24 } }"
             ;;
         tbf)
-            echo ", \"tca-options\": { \"parms\": \"$(perl -e 'print "00" x 36')\",
+            echo ", \"options\": { \"parms\": \"$(perl -e 'print "00" x 36')\",
                                        \"burst\": 1000 }"
             ;;
         *)
@@ -92,8 +91,8 @@ do
          --spec Documentation/netlink/specs/tc.yaml \
          --create --replace \
          --do newqdisc --json "{
-             \"tcm-ifindex\": ${INDEX}, \"tcm-parent\": $(parent ${qdisc}),
-             \"tca-kind\": \"${qdisc}\"$(options ${qdisc})
+             \"ifindex\": ${INDEX}, \"parent\": $(parent ${qdisc}),
+             \"kind\": \"${qdisc}\"$(options ${qdisc})
              }" 2>&1
 done
 true
